99fb70e7b6512994aa2393ea09076ed7a1cd6da0
user <user@DESKTOP-USH407P>
2021-01-12 09:45:28 +0900

    ...

---
+++ ver00.00/Hello/src/org/yjj/chapter/s015/JavaPrimaryType.java
@@ -0,0 +1,13 @@
+package org.yjj.chapter.s015;
+
+public class JavaPrimaryType {
+
+	public static void main(String[] args) {
+		boolean isL=false;
+		char suit='H';
+		int year=2016;
+		double latitude=37.52127220511242;
+		double longitude=127.0074462890625;
+		System.out.println(latitude+"\t"+longitude);
+	}
+}
