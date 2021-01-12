4104f8ef6efbbe3da6210cfe501bfa3cb53d83ce
user <user@DESKTOP-USH407P>
2021-01-12 10:05:49 +0900

    ...

--- ver00.00/Hello/src/org/yjj/chapter/s018/JavaTypeCoversion.java
+++ ver00.00/Hello/src/org/yjj/chapter/s018/JavaTypeCoversion.java
@@ -4,8 +4,7 @@ public class JavaTypeCoversion {
 
 	public static void main(String[] args) {
 		String slat=" 37.52127220511242     ";
-		//String slat="hello";
-		//공백 제거 후 double로 타입 변환
+	
 		double latitude=Double.parseDouble(slat.trim()); 
 		System.out.println(latitude);
 	}	
